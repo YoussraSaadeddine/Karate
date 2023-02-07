965503890b5026f01489388980e5b2a4a1a95d57
ysaadedd <ysaadedd@LMA001963.corp.capgemini.com>
2023-02-07 17:44:19 +0100

    pom commit

--- Karate/src/test/java/tests/APIRunner.java
+++ Karate/src/test/java/tests/APIRunner.java
@@ -2,10 +2,17 @@ package tests;
 
 import org.junit.runner.RunWith;
 
+
 import com.intuit.karate.junit4.Karate;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
 @RunWith(Karate.class)
+@CucumberOptions(plugin = {"pretty","html:target/rapport.html","json:target/cucumber.json"})
 
 public class APIRunner {
+	
 
 }
